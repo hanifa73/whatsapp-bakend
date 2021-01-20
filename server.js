890011d083a697c50dbec25482ac290d09c651df
hanifa73 <akkihanifa@gmail.com>
2021-01-20 23:04:25 +0300

    untracked node_modules

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import cors from "cors";
 
 //app config
 const app=express();
-const port=process.env.PORT || 9000
+const port=process.env.PORT || 9000;
 //  const port= process.env.PORT || 'https://whatsapp-backend-me.herokuapp.com'; 
 const pusher = new Pusher({
   appId: "1140631",
@@ -88,7 +88,8 @@ app.post('/messages/new', (req,res) => {
     })
 })
 //listen
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+app.listen(process.env.PORT ||9000, () => console.log(`Example app listening at http://localhost:${port}`))
 
 
 
